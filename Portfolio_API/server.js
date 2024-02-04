620e5a6a5fbf0cc360070a53b3e9ed9c54ee377d
Patryk Mansfeld <patryk.mansfeld@gmail.com>
2024-02-04 16:02:43 +0100

    Create server.js

---
+++ Portfolio_API/server.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const education = require('./models/educationModel')
+
+//middleware
+app.use(express.json())
+
+//Get Routes
+app.get('/', (req, res) => {
+    res.send('Hello Node Api')
+})
+
+app.get('/info', (req, res) =>{
+    res.send('Hello info 2')
+})
+
+
+//Post Routes
+app.post('/education', async(req, res) => {
+    try {
+        const education = await education.create(req.body)
+        res.status(200).json(education);
+
+    } catch {
+        console.log(error.message)
+        res.status(500).json({message: error.message})
+    }
+})
+
+// Modify Routtes
+
+// Delete Routes
+
+mongoose.connect('')
+.then(() =>{
+    console.log('conected to mongoDB')
+    app.listen(3000, () => {
+        console.log("The Server is running on port 3000")
+    })
+}).catch((error) =>{
+    console.log('error:', error.message)
+}) 

5f4e17364a3c51e95298a579e43a6e786a0a76eb
Patryk_Mansfeld <patryk.mansfeld@gmail.com>
2024-04-26 12:37:53 +0200

    changes in education component

--- Portfolio_Frontend/src/Components/Education/Education.jsx
+++ Portfolio_Frontend/src/Components/Education/Education.jsx
@@ -1,13 +1,20 @@
-import React from "react";
+// import React from "react";
 
 function Education() {
   return (
     <div className="Education">
-      <div className="EducationBox">
-        <h1>Poznan University Of Economics</h1>
-        <p>Field of study : Computer science</p>
-        <p>Specialization : Data Analysis</p>
-        <p>Dates: 10.2018 - 03.2020</p>
+      <div className="Education_uep">
+        <h1 className="edu_title">Poznan Economics <br/> University</h1>
+        <p className="edu_topic">Field of study <br/> Computer science</p>
+        <p className="edu_specialization">Specialization <br/> Data Analysis</p>
+        <p className="edu_date">10.2018 - 03.2020</p>
+      </div>
+
+      <div className="Education_cdv">
+        <h1 className="edu_title">Collegium Da Vinci</h1>
+        <p className="edu_topic">Field of study <br/> Computer science and Economics</p>
+        <p className="edu_specialization">Specialization <br/> Web Applications</p>
+        <p className="edu_date">2020 - 2023</p>
       </div>
     </div>
   );
--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # PM_Portfolio_Website
- 

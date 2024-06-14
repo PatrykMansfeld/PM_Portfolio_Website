c77dae8cdf908a3980bafd1b6393ea86582892be
Patryk_Mansfeld <patryk.mansfeld@gmail.com>
2024-06-14 17:35:21 +0200

    Landing Page

---
+++ Portfolio_Frontend/src/Components/LandingPage/LandingPage.jsx
@@ -0,0 +1,11 @@
+// import React from 'react';
+
+const LandingPage = () => {
+    return (
+        <div className="LP-Container">
+            <h1></h1>
+        </div>
+    );
+};
+
+export default LandingPage;

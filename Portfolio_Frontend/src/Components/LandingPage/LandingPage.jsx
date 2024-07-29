6efdfed3157eba2954c2d3ccd7ab816be193629a
Patryk_Mansfeld <patryk.mansfeld@gmail.com>
2024-07-29 17:16:30 +0200

    1

--- Portfolio_Frontend/src/Components/LandingPage/LandingPage.jsx
+++ Portfolio_Frontend/src/Components/LandingPage/LandingPage.jsx
@@ -1,14 +1,23 @@
-// import React from 'react';
+import { Link } from "react-router-dom";
 
 const LandingPage = () => {
   return (
     <div className="LP-Container">
       <div className="Initials">
-        <h1>Patryk <br/> Mansfeld</h1>
+        <h1>
+          Patryk <br /> Mansfeld
+        </h1>
         <h1>Fullstack Developer</h1>
       </div>
       <div className="LP-AboutMe">
-        <h1>About Me</h1>
+        <Link to="/aboutme" className="aboutme-link">
+          <h1>About Me</h1>
+        </Link>
+      </div>
+      <div className="LP-Projects">
+        <Link to="/projects" className="projects-link">
+          <h1>Projects</h1>
+        </Link>
       </div>
     </div>
   );
